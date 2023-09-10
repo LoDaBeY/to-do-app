fee5ed1dc0d789d9dc98e27a3078c998742999bf
Khaled <khaledonem89@gmail.com>
2023-09-10 22:11:25 +0300

    Finished

--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToDoList.css";
 import CloseIcon from "@mui/icons-material/Close";
 import EditIcon from "@mui/icons-material/Edit";
-let textid = document.querySelector("#textid");
 
-function ToDoList() { 
-  const SubmitTask = () => {
-    
-  }
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
 
-  const Delete = () => {
-    textid.value = "";
-  }
+  const handleInputChange = (e) => {
+    setTaskInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskInput.trim() !== "") {
+      setTasks([...tasks, taskInput]);
+      setTaskInput("");
+    }
+  };
+
   return (
-    <section class="boxes">
-      <div class="todolist">
-        <div className="Name">
-          <p>Khaled Mohamed Elsayed Ghonim</p>
-        </div>
-        <div className="Icons">
-          <div>
-            <CloseIcon className="CloseIcon" />
+    <section className="boxes">
+      {tasks.map((task, index) => (
+        <div className="todolist" key={index}>
+          <div className="Name">
+            <p>{task}</p>
           </div>
-          <div>
-            <EditIcon className="EditIcon" />
+          <div className="Icons">
+            <div>
+              <CloseIcon className="CloseIcon" />
+            </div>
+            <div>
+              <EditIcon className="EditIcon" />
+            </div>
           </div>
         </div>
-      </div>
-      <form className="submit">
-        <input type="text" name="enter" id="textid" placeholder=" Write here" />
-        <input type="button" defaultValue="create" id="submitt" onClick={SubmitTask} />
+      ))}
+      
+      <form className="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="enter"
+          id="textid"
+          placeholder="Write here"
+          value={taskInput}
+          onChange={handleInputChange}
+        />
+        <input type="submit" value="Create"     id="submitt"/>
       </form>
     </section>
   );
